3367cee2ed373e1d67da8821fcd8bfc0bc2283f5
BilyanaMarkova05 <92872724+BilyanaMarkova05@users.noreply.github.com>
2022-10-17 11:49:11 +0300

    implement AuthorshipDetector methods

--- Authorship detection/src/main/java/controller/AuthorshipDetectorImpl.java
+++ Authorship detection/src/main/java/controller/AuthorshipDetectorImpl.java
@@ -7,4 +7,19 @@ public class AuthorshipDetectorImpl implements AuthorshipDetector{
     public AuthorshipDetectorImpl(InputStream signaturesDataset, double[] weights){
 
     }
+
+    @Override
+    public LinguisticSignature calculateSignature(InputStream mysteryText) {
+        return null;
+    }
+
+    @Override
+    public double calculateSimilarity(LinguisticSignature firstSignature, LinguisticSignature secondSignature) {
+        return 0;
+    }
+
+    @Override
+    public String findAuthor(InputStream mysteryText) {
+        return null;
+    }
 }
